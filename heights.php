b86f9c06b13b6c2aaf00486ce70f2925f9fb09d3
Ralph Hughes <ralphhughes@hotmail.com>
2023-02-15 11:11:11 +0000

    Moved some files into resources sub folder. Added location drop down and date picker

--- heights.php
+++ heights.php
@@ -1,12 +1,23 @@
 <?php
-	// This API endpoint outputs a CSV of the tide height every 15 minutes for the next 24 hours.
+	error_reporting(E_STRICT);
+	// This API endpoint outputs a CSV of the tide height every 15 minutes for the given location and day
 	// Columns:
 	// UnixTimestamp<Long>, TideHeight<Float>
 	
+	// Strip any control characters from location then escape it (will single quote it)
+	$location = escapeshellarg(preg_replace('/[\x00-\x1F\x7F]/u', '', $_GET['location']));
+	
+	// Strip everything except numbers and dashes from timestamp (date)
+	$timestamp = escapeshellarg(preg_replace("/[^\:\ \d-]/i", "", $_GET['timestamp'] . " 00:00"));
+	
+	
 	// Command line for xtide
 	// Location:Llandudno, ExcludeMetadata:PhaseSunriseSunsetMoonriseMoonset, StepInterval:15min, PeriodToGenerateLength:1Day, Mode:RawUnixTimestamps, Format:CSV
-	$cmd = 'tide -l "Llandudno, Gwynedd, Wales" -em pSsMm -s "00:15" -pi 1 -m r -f c';
+	$cmd = 'tide -l ' . $location . ' -b ' . $timestamp . ' -em pSsMm -s "00:15" -pi 1.5 -m r -f c';
 	
+	// Debug
+	// print($cmd . "\n");
+
 	$descriptorspec = array(
 	0 => array("pipe", "r"),   // stdin is a pipe that the child will read from
 	1 => array("pipe", "w"),   // stdout is a pipe that the child will write to
@@ -15,6 +26,7 @@
 	
 	$process = proc_open($cmd, $descriptorspec, $pipes, realpath('./'), array());
 	
+	// $rows[] = [];
 	if (is_resource($process)) {
 		while ($line = fgets($pipes[1])) {
 			$myArr = explode(',', $line);	// 0 is location, 1 is unix timestamp, 2 is tide height
@@ -25,8 +37,10 @@
 			
 			$myArr[2] = preg_replace('/[\x00-\x1F\x7F]/u', '', $myArr[2]) + 0; // replace any control characters from the string and convert to number
 			
+			$myArr[2] = number_format((float)$myArr[2], 2, '.', ''); // Format to 2 decimal places always
+			
 			// Add to rows
-			$rows[]=array($myArr[1] + 0, $myArr[2]);
+			$rows[]=array($myArr[1], $myArr[2]);
 		}	
 	}
 	
