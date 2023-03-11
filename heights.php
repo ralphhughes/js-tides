41c612ee385e74d3772f35959c1c06229146e1e5
Ralph Hughes <ralphhughes@hotmail.com>
2023-03-11 09:44:24 +0000

    Added desktop interface, work-in-progress. Also fixed bug where chart was defaulting to today even if user chose another date

--- heights.php
+++ heights.php
@@ -5,26 +5,47 @@
 	// Columns:
 	// UnixTimestamp<Long>, TideHeight<Float>
 	
+	
+	function array2csv($data, $delimiter = ',', $enclosure = '"', $escape_char = "\\") {
+		$f = fopen('php://memory', 'r+');
+		foreach ($data as $item) {
+			fputcsv($f, $item, $delimiter, $enclosure, $escape_char);
+		}
+		rewind($f);
+		return stream_get_contents($f);
+	}
+	
 	// Strip any control characters from location then escape it (will single quote it)
 	$location = escapeshellarg(preg_replace('/[\x00-\x1F\x7F]/u', '', $_GET['location']));
 	
 	// Strip everything except numbers,dashes, colons and spaces from timestamp (ISO8601 date) and escape it
 	$timestamp = escapeshellarg(preg_replace("/[^\:\ \d-]/i", "", $_GET['timestamp'] . " 00:00"));
-
+	
 	// Convert days to integer greater than zero
 	$numDays = (int) $_GET['days'];
 	if ($numDays < 1 || $numDays > 366) {
 		$numDays = 1;
 	}
 	
-
+	if ($numDays == 1) {
+		$interval = "00:15";
+	} else {
+		$interval = "00:30";
+	}
+	
+	if (strcasecmp($_GET['format'], "csv") == 0) {
+		$format = "csv";
+	} else {
+		$format = "json";
+	}
+	
 	// Command line for xtide
 	// Location:Llandudno, ExcludeMetadata:PhaseSunriseSunsetMoonriseMoonset, StepInterval:15min, PeriodToGenerateLength:1Day, Mode:RawUnixTimestamps, Format:CSV
-	$cmd = 'tide -l ' . $location . ' -b ' . $timestamp . ' -em pSsMm -s "00:15" -pi ' . $numDays . ' -m r -f c';
+	$cmd = 'tide -l ' . $location . ' -b ' . $timestamp . ' -em pSsMm -s "' . $interval . '" -pi ' . $numDays . ' -m r -f c';
 	
 	// Debug
 	// print($cmd . "\n");
-
+	
 	$descriptorspec = array(
 	0 => array("pipe", "r"),   // stdin is a pipe that the child will read from
 	1 => array("pipe", "w"),   // stdout is a pipe that the child will write to
@@ -51,6 +72,11 @@
 		}	
 	}
 	
-	// Turn the rows variable into json
-	echo json_encode($rows);
+	// Turn the rows array into csv or json for returning to client
+	if ($format == "csv") {
+		echo "unix_time,height\n";
+		echo array2csv($rows);
+	} else if ($format == "json") {
+		echo json_encode($rows);
+	}
 	flush();
